4b112c5e55d572ee3cbf04a070aceb820b41ed90
radenmarcell <114900132+radenmarcell@users.noreply.github.com>
2023-09-27 13:00:43 +0700

    first commit

---
+++ jurnal01.java
@@ -0,0 +1,7 @@
+public class jurnal01{
+    public static void main(String[] args) {
+        System.out.println("Nama  : " + "Marcello Dic Zigo");
+        System.out.println("Nim   : " + "6706220140");
+        System.out.println("Kelas :" + " 47-01");
+    }
+}
